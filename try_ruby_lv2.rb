176f6a145975b65538640426d3842fb2bf47ee7d
Zia'ul Haq <ziaulhaq@mhs.dinus.ac.id>
2017-07-16 22:44:38 +0800

    try ruby lv2

---
+++ try_ruby_lv2.rb
@@ -0,0 +1,21 @@
+#link = http://tryruby.org/levels/2/challenges/1
+
+# chalengge 1
+# 40.reverse is method error, because 40 is number, basically, number cant reverse. need to convert into specific datatype and then reverse it
+
+40.to_s.reverse
+
+# learn bracket
+[]
+[12, 47, 35]
+
+#then get one is the highest in the array
+[12, 47, 35].max
+
+# assign the array into var
+ticket = [12, 47, 35]
+
+
+
+
+
