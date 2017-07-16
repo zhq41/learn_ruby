4344f4c4e79d14e99d294592ebe282c9627c62e7
Zia'ul Haq <ziaulhaq@mhs.dinus.ac.id>
2017-07-16 22:45:52 +0800

    try ruby lv1

---
+++ try_ruby_lv1.rb
@@ -0,0 +1,29 @@
+#level 1
+
+#link = https://www.codeschool.com/courses/try-ruby
+
+# challenge 1
+# learn add
+2 + 6 
+
+# another mathematic operator
+4 * 2
+5 - 12
+40 / 4
+
+# challenge 2
+# say your name
+
+"jimmy"
+
+# reverse that
+"jimmy".reverse
+
+# counting the letters
+"jimmy".length
+
+# on repeat
+"jimmy"*5
+
+
+
